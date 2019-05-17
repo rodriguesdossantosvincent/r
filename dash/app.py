54e6f0a8014935a9b18a311db794d31b89bca052
RODRIGUES DOS SANTOS Vincent <vincent@phec.net>
2019-05-17 14:15:49 +0200

    extension get url

--- dash/app.py
+++ dash/app.py
@@ -1,16 +1,9 @@
 # coding: utf-8
 
-from ipykernel.comm import Comm
-from urllib.parse import urlparse
-import time
-
-dash_comm = Comm(target_name='dash_viewer')
-jupyterlab_url = None
-
 
 import dash
 import dash_html_components as html
-
+import json
 
 app = dash.Dash(__name__)
 
@@ -19,25 +12,9 @@ app.layout = html.Div([
     html.Div('page-content')
 ])
 
-
-@dash_comm.on_msg
-def _recv(msg):
-    global jupyterlab_url
-    msg_data = msg.get('content').get('data')
-    msg_type = msg_data.get('type', None)
-    if msg_type == 'url_response':
-        jupyterlab_url = msg_data['url']
-
-
 if __name__ == '__main__':
-    dash_comm.send({
-        'type': 'url_request'
-    })
-    
-    while jupyterlab_url is None:
-        time.sleep(2)
-
-    path = urlparse(jupyterlab_url).path
+    path = json.load(open('/tmp/url.json', 'r')).get('base_url')
+    print('path', path)
     app.config.update({
         'requests_pathname_prefix': f'{path}proxy/8050/'})
     app.run_server(debug=True)
--- dashserverextension.py
+++ dashserverextension.py
@@ -1,7 +1,9 @@
 from subprocess import Popen
-
+import json
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
     #Popen(["cd", "dash", "&&", "python", "app.py"])
+    web_app = nbapp.web_app
+    open('/tmp/url.json', 'w').write(json.dumps({'base_url': {web_app.settings['base_url']}}))
     Popen(["python", "app.py"], cwd="dash")
