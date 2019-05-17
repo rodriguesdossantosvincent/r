29bcf888d110c371843a1b35c1532f6d1ea35c47
RODRIGUES DOS SANTOS Vincent <vincent@phec.net>
2019-05-17 14:19:06 +0200

    path ?

--- dashserverextension.py
+++ dashserverextension.py
@@ -5,5 +5,5 @@ def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
     #Popen(["cd", "dash", "&&", "python", "app.py"])
     web_app = nbapp.web_app
-    open('/tmp/url.json', 'w').write(json.dumps({'base_url': {web_app.settings['base_url']}}))
+    open('/tmp/url.json', 'w').write(json.dumps({'base_url': web_app.settings['base_url']}))
     Popen(["python", "app.py"], cwd="dash")
